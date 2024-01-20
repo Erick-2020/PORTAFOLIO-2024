130b4b2781268c8b2c56ad9922fbba55298176de
new-page-rh <leonardoraerick20@gmail.com>
2024-01-19 20:56:21 -0500

    Hacemos build para subir al host netlify :technologist:

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,14 @@
+import Layout from '../layouts/Layout.astro';
+
+<Fragment>
+<Layout title="Welcome to Astro.">
+<main>
+<h1>Hey, soy Erick Ramirez <badge>Disponible para trabajar</badge></h1>
+<h2>+2 años de experiencia. Desarrollador Web Frontend y Broker.
+De Colombia, Villeta :smile
+</h2>
+</main>
+</Layout>
+
+
+</Fragment>;
